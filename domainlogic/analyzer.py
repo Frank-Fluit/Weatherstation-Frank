31440f027229d1de28684cb0b8dee5d6d90b3c7f
FrankFluit <frank.fluit@wur.nl>
2023-11-17 14:53:47 +0100

    added basic functionality

--- domainlogic/analyzer.py
+++ domainlogic/analyzer.py
@@ -1,3 +1,9 @@
+import numpy as np
+import pandas as pd
+
+
 def perform_analysis(csv_data):
-    # Your analysis logic here
-    pass
+    data = pd.read_csv(csv_data)
+    print(data)
+    print("we zijn in performanalysis")
+    return [data['windspeed'].mean()]
