d1940ba84058f1cd13254ae3bb724af8a6909606
FrankFluit <frank.fluit@wur.nl>
2023-11-23 16:23:27 +0100

    Set up the data models for temp and windspeed

--- djangobackend/dataprocessing/models.py
+++ djangobackend/dataprocessing/models.py
@@ -1,3 +1,16 @@
+
 from django.db import models
 
-# Create your models here.
+class Location(models.Model):
+    location = models.CharField(max_length=200)
+
+class WindSpeed(models.Model):
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    windspeed = models.IntegerField(default=0)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class Temperature(models.Model):
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    temperature = models.IntegerField(default=0)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
