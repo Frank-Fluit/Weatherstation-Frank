3ad40abe8cd597342e0df7e4e289271459088cef
FrankFluit <frank.fluit@wur.nl>
2023-11-30 09:12:13 +0100

    improved layout of frontend code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 import './App.css';
 import UserProfile from './components/UserProfile';
 import UserRegistrationForm from './components/UserRegistrationForm';
 import LoginPage from "./components/LoginPage.tsx";
-
-
-
-
 
 
 const App: React.FC = () => {
@@ -35,31 +30,3 @@ const App: React.FC = () => {
 export default App;
 
 
-// function App() {
-//
-//   return (
-//     <>
-//
-//       <h1 className="mb-4 text-8xl font-semibold">Personal Weather station</h1>
-//
-//         <img src={raincloud}
-//            style={{ width: '300px', height: 'auto' }}>
-//         </img>
-//
-//       <h2>Log, compare and analyse your weatherdata using our personal accounts!</h2>
-//
-//       <div className="card">
-//
-//         <LoginPage/>
-//
-//         <UserRegistrationForm />
-//
-//         <CsvUploadForm />
-//
-//       </div>
-//
-//     </>
-//   );
-// }
-//
-// export default App;
