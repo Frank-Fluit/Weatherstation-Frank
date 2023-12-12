c127817c54d4db90ac545b6f7d0f889562e171d6
FrankFluit <frank.fluit@wur.nl>
2023-12-12 17:12:27 +0100

    added new components to userprofile

--- client/src/components/UserProfile.tsx
+++ client/src/components/UserProfile.tsx
@@ -3,6 +3,9 @@ import SaveDataToDatabase from "./SaveDataToDatabase.tsx";
 import DataAnalysis from "./DataAnalysis.tsx";
 import DataComparedToDatabaseData from "./DataComparedToDatabaseData.tsx";
 import CorrelationCalculation from "./CorrelationCalculation.tsx";
+import GetLocalWeather from "./GetLocalWeather.tsx";
+import GetLocalWeatherByLocation from "./GetLocalWeatherByLocation.tsx";
+
 
 const UserProfile: React.FC = () => {
 
@@ -13,6 +16,9 @@ const UserProfile: React.FC = () => {
         <SaveDataToDatabase />
         <DataComparedToDatabaseData/>
         <CorrelationCalculation/>
+        <GetLocalWeather/>
+        <GetLocalWeatherByLocation/>
+
     </div>
   );
 };
