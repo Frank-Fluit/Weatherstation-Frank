a603d91010bb8ae7ef2d90f1ace6046c92088ea8
FrankFluit <frank.fluit@wur.nl>
2023-12-12 17:11:52 +0100

    Created component to get weather data from ex api by city name and country code

---
+++ client/src/components/GetLocalWeatherByLocation.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+
+
+
+const GetLocalWeatherByLocation = () => {
+  const [city, setCity] = useState('');
+  const [countryCode, setCountryCode] = useState('');
+
+
+  const handleRequest = async () => {
+    try {
+      const response = await fetch("api/dataprocessing/get_weather_based_on_loc/", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ city, countryCode }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+
+        console.log("request succesful");
+
+
+      } else {
+        console.error('request failed.');
+      }
+    } catch (error) {
+      console.error('Error during request:', error);
+    }
+  };
+
+  return (
+    <div>
+
+      <h2>Find local weather using City and Country input</h2>
+      <label style={{ fontWeight: 'bold' }}>
+         City/Village:
+        <input type="text" value={city} onChange={(e) => setCity(e.target.value)} />
+      </label>
+      <br />
+      <label style={{ fontWeight: 'bold' }}>
+        Country Code:
+        <input type="text" value={countryCode} onChange={(e) => setCountryCode(e.target.value)} />
+      </label>
+      <br />
+      <button onClick={handleRequest}>GetLocalWeather</button>
+
+    </div>
+  );
+};
+
+export default GetLocalWeatherByLocation;
+
