5ad52f687aaaac15eac6b5a1feef2dd7b7e943f7
FrankFluit <frank.fluit@wur.nl>
2023-12-14 16:23:40 +0100

    set up separate service module and refactor views

--- client/src/components/Vegalitetwo.tsx
+++ client/src/components/Vegalitetwo.tsx
@@ -1,21 +1,16 @@
 import React, {useState} from 'react';
 import { VegaLite } from 'react-vega';
 
-const Vegalite = () => {
+const Vegalitetwo = () => {
 
   const [latitude, setLatitude] = useState<string>('');
   const [longitude, setLongitude] = useState<string>('');
   const [data, setData] = useState<string | null>(null);
+  const [city, setCity] = useState('');
+  const [countryCode, setCountryCode] = useState('');
 
 
-  const timeSeriesData = [
-    { time: '2023-01-01T00:00:00Z', value: 8 },
-    { time: '2023-01-01T01:00:00Z', value: 9 },
-    { time: '2023-01-01T02:00:00Z', value: 11 },
-    { time: '2023-01-01T03:00:00Z', value: 12 },
-    { time: '2023-01-01T04:00:00Z', value: 15 },
 
-  ];
 
 
   const spec = {
@@ -30,23 +25,22 @@ const Vegalite = () => {
     title: 'Temperature prediction for next 5 days',
   };
 
-    const handleRequest = async () => {
+  const handleRequest = async () => {
     try {
-      const response = await fetch("api/dataprocessing/get_prediciton_based_on_lat_lon/", {
+      const response = await fetch("api/dataprocessing/get_weather_pred_loc/", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ latitude, longitude }),
+        body: JSON.stringify({ city, countryCode }),
       });
 
       if (response.ok) {
         const data = await response.json();
         setData(data)
 
-
         console.log("request succesful");
-        console.log(data)
+
 
       } else {
         console.error('request failed.');
@@ -58,19 +52,19 @@ const Vegalite = () => {
 
   return (
     <div>
-      <h2>Find weather forecast using coordinates</h2>
+      <h2>Find weather forecast using location name</h2>
       <VegaLite spec={spec} />
           <div>
 
 
       <label style={{ fontWeight: 'bold' }}>
-        Latitude:
-        <input type="text" value={latitude} onChange={(e) => setLatitude(e.target.value)} />
+        City:
+        <input type="text" value={city} onChange={(e) => setCity(e.target.value)} />
       </label>
       <br />
       <label style={{ fontWeight: 'bold' }}>
-        Longitude:
-        <input type="text" value={longitude} onChange={(e) => setLongitude(e.target.value)} />
+        countryCode:
+        <input type="text" value={countryCode} onChange={(e) => setCountryCode(e.target.value)} />
       </label>
       <br />
       <button onClick={handleRequest}>TempPrediction</button>
@@ -82,4 +76,4 @@ const Vegalite = () => {
   );
 };
 
-export default Vegalite;
+export default Vegalitetwo;
